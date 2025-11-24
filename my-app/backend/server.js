fbea13ac04cb024e02de044e22dd04f940e24109
Jorge12 <0250990@up.edu.mx>
2025-11-23 18:50:33 -0600

    Agrego ruta base

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -14,6 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend funcionando en Render");
+});
 app.use("/api/users", userRoutes);
 app.use("/api/games", gameRoutes);
 app.use("/api/scores", scoreRoutes);
